91c7d4a44bf88b4042d17f4b682437ac1863b883
julian.castellanos02 <julian.castellanos02@uptc.edu.co>
2021-03-15 14:46:31 -0500

    Adding new changes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import { loadStripe } from "@stripe/stripe-js";
 // Elements nos permite englobar a otros componentes para cada componente que contenga pueda tener acceso a la conexion de stripe
 // useStripe nos retorna la conexion con stripe y useElements nos permite acceder a los elementos de stripe
 import { Elements } from "@stripe/react-stripe-js";
-import { Cards } from "./components/cards/Cards";
+import { CheckoutForm } from "./components/CheckoutForm";
 
 // recibe como parametro una llave publica , contraseña que da stripe y nos permite hacer la conexion con stripe
 const stripePromise = loadStripe(
   "pk_test_51IV6wFD9svJa4NXzFHG11h8pjaRilG8g07wZSE73jx6fxnaMfWxmAdTWleDqrZ3CZxXg0q9C4niFJPMrGYFeszxF00gvt1Ykpe"
 );
 
-function App() {
+const ELEMENTS_OPTIONS = {
+  fonts: [
+    {
+      cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
+    },
+  ],
+};
+const App = () => {
   return (
-    // pasamos por props la conexion que se heredara a todos los componentes hijos de element
-    <Elements stripe={stripePromise}>
-      <Cards />
-    </Elements>
+    <div className="AppWrapper">
+      <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
+        <CheckoutForm />
+      </Elements>
+    </div>
   );
-}
+};
 
 export default App;
